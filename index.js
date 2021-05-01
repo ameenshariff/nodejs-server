6af9b48d2e00eed82b608d8e452b365d452a787d
ameshari <ameenshariff0880@gmail.com>
2021-05-01 17:56:24 +0530

    added get route

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+const PORT = 3000
+
+app.listen(PORT, () => console.log(`Server is running at port ${PORT}`))
+
+app.get('/', (req, res) => {
+    res.send("<h1>Hello World</h1>");
+})
